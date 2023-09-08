a09cc404ee1c7cd8a788e7b03160fe61e35c419e
rudol <rudol@10.41.111.0>
2023-09-08 04:28:59 +0300

    Update

--- src/mc/rellox/spawnermeta/hook/HookRegistry.java
+++ src/mc/rellox/spawnermeta/hook/HookRegistry.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HookRegistry {
-	
+
 	private static final List<HookInstance<?>> HOOKS = new ArrayList<>();
-	
-    public static final HookEconomy ECONOMY = new HookEconomy();
-    public static final HookWildStacker WILD_STACKER = new HookWildStacker();
-    public static final HookWildTools WILD_TOOLS = new HookWildTools();
-    public static final HookShopGUI SHOP_GUI = new HookShopGUI();
-    public static final HookFlareTokens FLARE_TOKENS = new HookFlareTokens();
-	
+
+	public static final HookEconomy ECONOMY = new HookEconomy();
+	public static final HookWildStacker WILD_STACKER = new HookWildStacker();
+	public static final HookWildTools WILD_TOOLS = new HookWildTools();
+	public static final HookShopGUI SHOP_GUI = new HookShopGUI();
+	public static final HookFlareTokens FLARE_TOKENS = new HookFlareTokens();
+
 	public static void load() {
 		HOOKS.add(ECONOMY);
 		HOOKS.add(WILD_STACKER);
