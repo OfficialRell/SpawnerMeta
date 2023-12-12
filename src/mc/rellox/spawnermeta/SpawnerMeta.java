53592aec15c0c4519cf4ef007c0bf121307d5113
BuildTools <unconfigured@null.spigotmc.org>
2023-12-13 00:17:37 +0200

    Version update

--- src/mc/rellox/spawnermeta/SpawnerMeta.java
+++ src/mc/rellox/spawnermeta/SpawnerMeta.java
@@ -25,7 +25,7 @@ import mc.rellox.spawnermeta.version.Version;
 
 public final class SpawnerMeta extends JavaPlugin {
 	
-	public static final double PLUGIN_VERSION = 22.3;
+	public static final double PLUGIN_VERSION = 22.4;
 	
 	private static SpawnerMeta plugin;
     
