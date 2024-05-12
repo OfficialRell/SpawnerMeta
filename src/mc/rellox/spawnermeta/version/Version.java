8ad968d58075b7028a3fcfcfcdd22b5118a750a1
BuildTools <unconfigured@null.spigotmc.org>
2024-05-13 02:13:06 +0300

    New spigot version support

--- src/mc/rellox/spawnermeta/version/Version.java
+++ src/mc/rellox/spawnermeta/version/Version.java
@@ -13,7 +13,8 @@ public final class Version {
 	static {
 		String s = Bukkit.getServer().getClass().getPackage().getName();
 		server = s.substring(s.lastIndexOf('.') + 1);
-		if(server.contains("v1_20_R3") == true) version = VersionType.v_20_3;
+		if(server.contains("v1_20_R4") == true) version = VersionType.v_20_4;
+		else if(server.contains("v1_20_R3") == true) version = VersionType.v_20_3;
 		else if(server.contains("v1_20_R2") == true) version = VersionType.v_20_2;
 		else if(server.contains("v1_20_R1") == true) version = VersionType.v_20_1;
 		else if(server.contains("v1_19_R3") == true) version = VersionType.v_19_3;
@@ -39,7 +40,7 @@ public final class Version {
 		v_17_1,
 		v_18_1, v_18_2,
 		v_19_1, v_19_2, v_19_3,
-		v_20_1, v_20_2, v_20_3;
+		v_20_1, v_20_2, v_20_3, v_20_4;
 		
 		public boolean high(VersionType type) {
 			return ordinal() >= type.ordinal();
