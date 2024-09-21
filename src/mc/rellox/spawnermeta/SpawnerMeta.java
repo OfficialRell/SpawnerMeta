256e7c7f36de033e9936c6624c83b9101f2b7a08
Rellox <unconfigured@null.spigotmc.org>
2024-09-21 22:45:35 +0300

    Updated plugin version

--- src/mc/rellox/spawnermeta/SpawnerMeta.java
+++ src/mc/rellox/spawnermeta/SpawnerMeta.java
@@ -25,7 +25,7 @@ import mc.rellox.spawnermeta.version.Version;
 
 public final class SpawnerMeta extends JavaPlugin {
 	
-	public static final double PLUGIN_VERSION = 24.5;
+	public static final double PLUGIN_VERSION = 24.6;
 	
 	private static SpawnerMeta plugin;
     
