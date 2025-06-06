7bf3fc8ecfdd282dfab2c8c7f1533899fc180aa1
adabugra <57899270+adabugra@users.noreply.github.com>
2025-06-06 12:07:29 +0300

    Use Collections#synchronizedList

--- src/mc/rellox/spawnermeta/spawner/generator/SpawnerWorld.java
+++ src/mc/rellox/spawnermeta/spawner/generator/SpawnerWorld.java
@@ -1,10 +1,6 @@
 package mc.rellox.spawnermeta.spawner.generator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
